0dfc36208f07c0e6ade503fe06cd13ffba15d31e
Dan Woo <daniel4wood@icloud.com>
2020-01-06 21:16:09 +0100

    Just build the whole damn site.

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
-module.exports = () => ({
-  plugins: [require("tailwindcss")],
-})
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./**/*.html"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+const cssnano = require("cssnano");
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    process.env.NODE_ENV === "production"
+      ? cssnano({ preset: "default" })
+      : null
+  ]
+};
--- src/pages/404.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-const NotFoundPage = () => <div>404 - Page not found</div>
-
-export default NotFoundPage
