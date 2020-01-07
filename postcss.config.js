10f7180593dadc6352876a6dd90c6f5bd3cc4256
Dan Wood <daniel4wood@icloud.com>
2020-01-07 12:53:35 +0100

    PurgeCSS tweaks
    Purge tailwind base and component styles to trim down normalize.
    
    Change order of autoprefixer so it doesn't get purged.
    
    Don't write "code" in text because then purgecss doesn't purge the
    code tags 🤦‍♀️

--- postcss.config.js
+++ postcss.config.js
@@ -10,8 +10,8 @@ const cssnano = require("cssnano");
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("autoprefixer"),
     ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    require("autoprefixer"),
     process.env.NODE_ENV === "production"
       ? cssnano({ preset: "default" })
       : null
